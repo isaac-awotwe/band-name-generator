6a9451c0bdeed97c54c169938c456a7a40510117
Isaac Awotwe <88563018+isaac-awotwe@users.noreply.github.com>
2024-03-12 01:56:37 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# band-name-generator
+## Band Name Generator App
+
+Suppose a friend was starting a new band (e.g. a rock band or a classical band) and they're struggling to come up with a good name.
+The band name generator program to the rescue!
+
+The band name generator is a simple application that asks a user for the city they grew up in and the name of their pet.
+The app then combines the city and pet names together as a suggestion of the name of the band.
+
+The app uses Python skills like:
+- printing to the console
+- commenting
+- the input function
+- python variables
+- string manipulation
+- debuggiing, etc.
+- streamlit
+
+Click on the link below to use the streamlit app.
---
+++ band_name_generator.py
@@ -0,0 +1,44 @@
+""" 
+The Band Name Generator Application powered by Streamlit
+"""
+
+import streamlit as st
+
+if 'stage' not in st.session_state:
+    st.session_state.stage = 0
+
+def set_state(i):
+    st.session_state.stage = i
+
+if st.session_state.stage == 0:
+    st.write("Click the button below to begin")
+    st.button('Begin', on_click=set_state, args=[1])
+
+if st.session_state.stage >= 1:
+    st.write("## Welcome to the Band Name Generator.")
+    name = st.text_input('What is your name?', on_change=set_state, args=[2])
+
+
+if st.session_state.stage >=2:
+    st.write(f'Hello {name.capitalize()}!')
+    city = st.text_input(
+        "Which city did you grew up in?",
+        on_change = set_state, args=[3]
+        )
+    if city is None:
+        set_state(2)
+
+#if st.session_state.stage == 3:
+#   st.button("Next", on_click=set_state, args=[4])
+
+if st.session_state.stage >=3:
+    pet = st.text_input(
+        "What is the name of a pet?",
+        on_change = set_state, args = [4])
+    if pet is None:
+        set_state(3)
+
+if st.session_state.stage >=4:
+    band_name = city.capitalize() + " " + pet.capitalize()
+    st.markdown(f"##### Your band name could be :orange[{band_name}]")
+    st.button("Start Over", on_click=set_state, args=[0])
